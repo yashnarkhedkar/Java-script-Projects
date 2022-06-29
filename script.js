33b4ed63d47802a4c70a40daf406db52b13d7121
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-06-29 17:59:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// *************************** Challenge #1 *********************************
+
 function calNum(year)
 {
     return parseInt(year / 4, 10) - parseInt(year / 100, 10) + parseInt(year / 400, 10);
@@ -20,8 +22,29 @@ function ageIndays() {
     let leapYears = leapNum(age, year);
     let days = ((year - age)* 365) + leapYears;
     document.getElementById('main').innerHTML = "You are " + days + " days old";
+    document.getElementById('reset').style.display = "inline";
 }
 
 function reset(){
     document.getElementById('main').style.display = "none";
+}
+
+// *************************** Challenge #2 *********************************
+
+let genrateBtn = document.getElementById('gen-btn');
+let main = document.getElementById('main2');
+let timeValue = null;
+let stop1 = document.getElementById('stop');
+
+function genrateImages(){
+    var image = document.createElement('img');
+    image.style.margin = "10px";
+    image.src = "https://picsum.photos/200?" + new Date().getTime();
+    main.appendChild(image);
+    timeValue = setTimeout(genrateImages, 1000);
+    stop1.style.display = "inline";
+}
+
+function stop(){
+    clearTimeout(timeValue);
 }
