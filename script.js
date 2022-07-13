a0c75be415c6bd779fd9220112bbf956cf44484a
Yash Narkhedkar <yashnarkhedkar@gmail.com>
2022-07-13 17:49:01 +0530

    13_07_2022

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 // *************************** Challenge #1 *********************************
 
-function calNum(year)
-{
+function calNum(year) {
     return parseInt(year / 4, 10) - parseInt(year / 100, 10) + parseInt(year / 400, 10);
 }
 
 // Function to calculate the number
 // of leap years in given range
-function leapNum(l, r)
-{
+function leapNum(l, r) {
     l--;
     let num1 = calNum(r);
     let num2 = calNum(l);
     return num1 - num2;
 }
 
-function ageIndays() { 
-    document.getElementById('main').style.display = "block";   
+function ageIndays() {
+    document.getElementById('main').style.display = "block";
     let age = prompt("Enter your age in years ");
     let year = new Date().getFullYear();
     let leapYears = leapNum(age, year);
-    let days = ((year - age)* 365) + leapYears;
+    let days = ((year - age) * 365) + leapYears;
     document.getElementById('main').innerHTML = "You are " + days + " days old";
     document.getElementById('reset').style.display = "inline";
 }
 
-function reset(){
+function reset() {
     document.getElementById('main').style.display = "none";
 }
 
@@ -36,7 +34,7 @@ let main = document.getElementById('main2');
 let timeValue = null;
 let stop1 = document.getElementById('stop');
 
-function genrateImages(){
+function genrateImages() {
     var image = document.createElement('img');
     image.style.margin = "10px";
     image.src = "https://picsum.photos/200?" + new Date().getTime();
@@ -45,24 +43,24 @@ function genrateImages(){
     stop1.style.display = "inline";
 }
 
-function stop(){
+function stop() {
     clearTimeout(timeValue);
 }
 
 // *************************** Challenge #3 *********************************
 var resetRps = document.getElementsByClassName('images');
 
-function botChoice(){
+function botChoice() {
     var num = Math.floor(Math.random() * 3);
     var arr = ['rock', 'paper', 'scissors'];
-    return arr[num]; 
+    return arr[num];
 }
 
-function decideWinner(human, bot){
+function decideWinner(human, bot) {
     var coditions = {
-        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
-        'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
-        'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0,}
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+        'scissors': { 'paper': 1, 'scissors': 0.5, 'rock': 0, }
     };
 
     var yourScore = coditions[human][bot];
@@ -71,24 +69,24 @@ function decideWinner(human, bot){
     return [yourScore, computerScore];
 }
 
-function finalMessage([yourScore]){
-    if(yourScore === 0){
-        return {'message': 'You lost!', 'color': 'red'};
+function finalMessage([yourScore]) {
+    if (yourScore === 0) {
+        return { 'message': 'You lost!', 'color': 'red' };
     }
-    else if(yourScore === 0.5){
-        return {'message': 'You Tied!', 'color': 'Yellow'};
+    else if (yourScore === 0.5) {
+        return { 'message': 'You Tied!', 'color': 'Yellow' };
     }
-    else{
-        return {'message': 'You Win!', 'color': 'Green'};
+    else {
+        return { 'message': 'You Win!', 'color': 'Green' };
     }
 }
 
-function rpsFrontEnd(human, bot, message){
+function rpsFrontEnd(human, bot, message) {
 
     var imageDatabase = {
-        'rock' : document.getElementById('rock').src,
-        'paper' : document.getElementById('paper').src,
-        'scissors' : document.getElementById('scissors').src,
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src,
     }
 
     //Remove all images
@@ -113,7 +111,7 @@ function rpsFrontEnd(human, bot, message){
     resetbtn.style.margin = "auto";
 }
 
-function rps(yourChoice){
+function rps(yourChoice) {
     var human = yourChoice.id;
     console.log("Your Choice: " + human);
 
@@ -130,11 +128,47 @@ function rps(yourChoice){
 }
 
 // *************************** Challenge #4 *********************************
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
+let btndiv = document.getElementsByClassName('four-btn')[0];
+let btns = btndiv.getElementsByTagName('button');
 
-let all_btns = document.getElementsByClassName('four-btns');
-let size = all_btns.length;
+function btncolorChange(buttonThing){
+    if(buttonThing.value === 'random'){
+        random();
+    } else if(buttonThing.value === 'red'){
+        red();
+    } else if(buttonThing.value === 'green'){
+        green();
+    } else{
+        reset();
+    }
+}
 
-for(let i = 0; i < size; i++){
-    all_btns[i].style.backgroundColor = toString("#" + randomColor);
+function random() {
+    let colors = ["#065535", "#ff0000", "#ffd700", "#ffa500",
+        "#ff7373", "#800080", "#800000", "#00ff00",
+        "#0e2f44", "#0e2f44", "#660066", "#420420",
+        "#3399ff", "#008744", "#ffa700", "#d62d20"];
+
+    for (let i = 0; i < btns.length; i++) {
+        let randomnum = Math.floor(Math.random() * 16);
+        let color = colors[randomnum];
+        btns[i].style.backgroundColor = `${color}`;
+    }
+}
+
+function red(){
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].style.backgroundColor = "red";
+    }
+}
+
+function green(){
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].style.backgroundColor = "green";
+    }
+}
+function reset(){
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].style.backgroundColor = "black";
+    }
 }
